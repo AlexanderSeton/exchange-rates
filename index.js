ff6cfce75225c2bf0871f92c1cc284410411c3cf
Alexander Seton <alexanderseton@protonmail.com>
2021-12-22 21:26:06 +0000

    Exchange rate function working, can now choose a date in last 90 days and get exchange rate between currencies from that date.

--- index.js
+++ index.js
@@ -15,14 +15,11 @@ async function fetchData() {
         const daysData = dData["Cube"];
         const tempOutput = [];
         const weekDate = dData["_attributes"]["time"];
-        // console.log(weekDate); // test
         tempOutput.push({"date": weekDate});
         for (let day of daysData) {
             const tempDay = day["_attributes"];
-            // console.log(tempDay); // test
             tempOutput.push(tempDay);
         }
-        // console.log(tempOutput); // test
         return tempOutput;
     });
     data = mappedData;
@@ -32,19 +29,68 @@ async function fetchData() {
 }
     
 // cron.schedule("0 1 * * *", function() {
-//     console.log("Running this function every day at 1am");
+//     console.log("Fetching new exchange rates data (fetch function executed every day at 1am)");
+//     fetchData();
 // })
 
 function exchangeRate(date, currency1, currency2) {
+    try {
+        date = date.substr(0, 10);
+        currency1 = String(currency1).toUpperCase();
+        currency2 = String(currency2).toUpperCase();
+        let specifiedDayData;
+        for (let dayData of data) {
+            if (dayData[0]["date"] === date) {
+                console.log("Found date:", date);
+                specifiedDayData = dayData;
+                // console.log(specifiedDayData)
+            }
+        }
+        for (let currencyData of specifiedDayData) {
+            if (currency1 === "EUR") {
+                if (currencyData["currency"] === currency2) {
+                    const rate = currencyData["rate"];
+                    return rate;
+                }
+            } else {
+                if (currencyData["currency"] === currency1) {
+                    let rate1 = currencyData["rate"];
+                    if (currency2 === "EUR") {
+                        const rate = 1 / parseFloat(currencyData["rate"]);
+                        return rate;
+                    } else {
+                        for (let currencyData of specifiedDayData) {
+                            if (currencyData["currency"] === currency2) {
+                                const rate2 = currencyData["rate"];
+                                rate1 = 1 / rate1;
+                                const rate = rate1 * rate2;
+                                return rate;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    } catch(error) { 
+        console.log(error);
+        return "Error: " + error;
+    }
 }
 
 function  convert(date, currency1, amount1, currency2, amount2) {
+    try {
+    } catch(error) { 
+        console.log(error);
+        return "Error: " + error;
+    }
 }
 
 // initial data fetch
 fetchData();
-// needed to view
+// // needed to view test
 setTimeout(() => {
-    console.log(data);
-    console.log(todayData);
+    // console.log(data);
+    // console.log(todayData);
+    console.log(exchangeRate("2021-12-22", "JPY", "RUB"));
 }, 2000)
