d134008de5dde2e83600941290de1f13028c7fd4
Alexander Seton <alexanderseton@protonmail.com>
2021-12-22 12:07:22 +0000

    Definied initial two function names, initialized npm and imported node-cron (for daily exchange rate data importing).

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const cron = require("node-cron");
+
+cron.schedule("0 1 * * *", function() {
+    console.log("Running this function every day at 1am");
+})
+
 function exchangeRate(date, currency1, currency2) {
 }
 
