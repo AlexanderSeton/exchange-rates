a07d2c960fe38566e2391ff84c730b5804d370fe
Alexander Seton <alexanderseton@protonmail.com>
2021-12-23 00:00:03 +0000

    When calling a function the program will check of it needs to update its data. Data is stored in a .json file.

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const cron = require("node-cron");
 const fetch = require("node-fetch");
 const convertData = require("xml-js");
+const fs = require("fs");
 
 let data;
 let todayData;
+
+// cron.schedule("15 0 * * *", function() {
+//     console.log("Fetching new exchange rates data (fetch function executed every day at 12:15 am).");
+//     fetchData();
+// })
+
+async function checkData() {
+    const rawFile = fs.readFileSync("data.json");
+    if (rawFile.toString() === "") {
+        await fetchData();
+    } else {
+        const parsedFile = JSON.parse(rawFile);
+        const mostRecentDate = parsedFile[0][0]["date"];
+        let today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
+        today = yyyy + '-' + mm + '-' + dd;
+        if (mostRecentDate !== today) {
+            console.log("out of date date");
+            await fetchData();
+        }
+    }
+}
 
 async function fetchData() {
     const response = await fetch("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml");
@@ -23,23 +48,28 @@ async function fetchData() {
         return tempOutput;
     });
     data = mappedData;
+    // save data to file
+    fs.writeFileSync("data.json", JSON.stringify(mappedData));
     // today's data
     const tempTodayData = await data[0];
     todayData = tempTodayData;
 }
-    
-// cron.schedule("0 1 * * *", function() {
-//     console.log("Fetching new exchange rates data (fetch function executed every day at 1am)");
-//     fetchData();
-// })
 
 function exchangeRate(date, currency1, currency2) {
     try {
+        checkData()
+        const rawFile = fs.readFileSync("data.json");
+        let dataObj = JSON.parse(rawFile);
+        const values = Object.values(dataObj);
+        const fileDataArray = [];
+        for (let value of values) {
+            fileDataArray.push(value);
+        }
         date = date.substr(0, 10);
         currency1 = String(currency1).toUpperCase();
         currency2 = String(currency2).toUpperCase();
         let specifiedDayData;
-        for (let dayData of data) {
+        for (let dayData of fileDataArray) {
             if (dayData[0]["date"] === date) {
                 // console.log("Found date:", date);
                 specifiedDayData = dayData;
@@ -78,7 +108,7 @@ function exchangeRate(date, currency1, currency2) {
     }
 }
 
-function  convert(date, currency1, currency2, amount) {
+function convert(date, currency1, currency2, amount) {
     try {
         const rate = exchangeRate(date, currency1, currency2);
         const output = amount * rate;
@@ -89,12 +119,7 @@ function  convert(date, currency1, currency2, amount) {
     }
 }
 
-// initial data fetch
-fetchData();
-// needed to view test
-setTimeout(() => {
-    // console.log(data);
-    // console.log(todayData);
-    console.log(exchangeRate("2021-12-22", "GBP", "USD"));
-    console.log(convert("2021-12-22", "GBP", "USD", 100));
-}, 2000)
+// testing
+console.log(exchangeRate("2021-12-22", "GBP", "EUR"));
+console.log(exchangeRate("2021-12-22", "GBP", "JPY"));
+console.log(convert("2021-12-21", "GBP", "USD", 500));
